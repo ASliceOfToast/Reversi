39c84a5b4699bc6b4c41995f998a146f73d20959
wizzardofsoz <whopointseven@gmail.com>
2016-02-29 23:40:48 -0500

    Update RandomComputerPlayer.java

--- RandomComputerPlayer.java
+++ RandomComputerPlayer.java
@@ -2,7 +2,7 @@ public class RandomComputerPlayer extends ComputerPlayer{
 
 public void setMove(){
 //will choose a random option from the int array that contains all possible moves
-//then use the setX setY methods
+//then sets position
 }
 
 }
