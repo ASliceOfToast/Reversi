82079cd4368a7af0a2fe45983f989ac9257f30e6
wizzardofsoz <whopointseven@gmail.com>
2016-03-01 20:48:56 -0500

    Added files via upload
    fleshed out player classes still don't know wtf to do with ComputerPlayer LOL maybe it's supposed to be blank, he said abstract classes are "placeholders" right

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 public class HumanPlayer extends Player{
 Scanner input = new Scanner(System.in);
+public int[][] possiblecoordinates;
+boolean ispossible;
 
-public void setMove(){
+public void setMove(int[][] possiblecoordinates){//will get possiblecoordinates from the whereCanIGo method in ReversiBoard
+   possiblecoordinates = this.possiblecoordinates;
    System.out.println("Enter your move, " + getPlayer() + " player: ");
    String fightme;
    fightme = input.nextLine().replaceAll(" ",""); //kill all white space
-     //not accounting for errors in player input yet =__=
+     //not accounting for syntax errors in player input yet =__=
    try{
       position[0] = Character.getNumericValue(fightme.charAt(0));
       position [1] = Character.getNumericValue(fightme.charAt(1));   
@@ -15,7 +18,18 @@ public void setMove(){
    catch(NumberFormatException curveball){
       //meh
    }
+   
+   for(int i = 0; i < possiblecoordinates.length;i++){
+         if(possiblecoordinates[i][0] == position[0] && possiblecoordinates[i][1] == position[1]){
+            ispossible = true;
+         }
+   }//for loop checks to see if the coordinates the user entered are valid moves/coordinates
+   
+   
+   if(!ispossible){
+      System.out.print("Invalid move.");
+      setMove(possiblecoordinates);
+   }//if the coordinates were invalid it runs the method again
 }
-
 
 }
--- RandomComputerPlayer.java
+++ RandomComputerPlayer.java
@@ -1,8 +1,12 @@
 public class RandomComputerPlayer extends ComputerPlayer{
+int rnum;
 
-public void setMove(){
-//will choose a random option from the int array that contains all possible moves
-//then sets position
+public void setMove(int[][] possiblecoordinates){
+   rnum = (int) Math.ceil(Math.random() * (possiblecoordinates.length - 1));
+   position[0] = possiblecoordinates[rnum][0]; 
+   position[1] = possiblecoordinates[rnum][1];
+   //will choose a random option from the int array that contains all possible moves
+//then set position
 }
 
 }
