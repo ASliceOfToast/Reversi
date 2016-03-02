0f8abd70f65db98e50502709064d725cedb0a5b4
wizzardofsoz <whopointseven@gmail.com>
2016-03-01 23:48:53 -0500

    Added files via upload

--- Reversi.java
+++ Reversi.java
@@ -1,9 +1,56 @@
 public class Reversi{
-
+int count = 1;
 
 
   public static void main(String[] args){
-   ReversiBoard gay = new ReversiBoard();
-   gay.printBoard();
+   
+   for(int i = 0;i<args.length;i++){
+         switch(args[i].toLowerCase()){
+            case "RandomComputerPlayer": 
+               if(i == 0){
+                  RandomComputerPlayer player1 = new RandomComputerPlayer("X");
+               }
+     
+               if(i == 1){
+                  RandomComputerPlayer player2 = new RandomComputerPlayer("O");
+               }
+            break;
+            case "Human":
+               if(i == 0){
+                  HumanPlayer player1 = new HumanPlayer("X");
+               }
+               if(i == 1){
+                  HumanPlayer player2 = new HumanPlayer("O");   
+               }
+            break;
+            default: 
+               System.out.println("Format is: java Reversi Player 1 Player 2. Replace Player 1 and Player 2 with Human or RandomComputerPlayer.");
+               System.exit(0);    
+            }//switch
+            
+   }//for loop
+   
+   System.out.println("\nWelcome to Reversi!  Moves should be entered in \"[row] [column]\" format.\n");
+   
+   
+   ReversiBoard itsaboard = new ReversiBoard();
+     
+   
+   //below commented out until boolean wincondition actually exists
+   /*while(!wincondition){
+         itsaboard.printBoard(); 
+         if((count%2 == 0){
+            player2.setMove(itsaboard.whereICanGo(player2.getPlayer()));
+         }
+         else{
+            player1.setMove(itsaboard.whereICanGo(player1.getPlayer()));
+         }
+   }
+   
+   some method that counts the amount of 'O's and 'X's on the board to determine winner
+   
+   */
+   
+   System.out.println("GG NUBS -insertplayer- WINS");
   }
 }
