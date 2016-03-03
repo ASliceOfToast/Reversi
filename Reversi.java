cdb32e1e9d016a4c98fb85a43aa84fa75e0061e5
wizzardofsoz <whopointseven@gmail.com>
2016-03-03 13:04:48 -0500

    Added files via upload

--- Reversi.java
+++ Reversi.java
@@ -11,11 +11,11 @@ public class Reversi{
   Player player2 = new Player(){
    public void setMove(ArrayList<int[]> screamssomemore){}
   };
-  int[] AHAHA = new int[2];
+  
    if(args.length >= 3){
       System.out.println("Format is: java Reversi Player 1 Player 2. Replace Player 1 and Player 2 with Human or RandomComputerPlayer.");
       System.exit(0);
-   }
+   }//if too many arguments
    
    for(int i = 0;i<args.length;i++){
          switch(args[i].toLowerCase()){
@@ -39,50 +39,46 @@ public class Reversi{
             default: 
                System.out.println("Format is: java Reversi Player 1 Player 2. Replace Player 1 and Player 2 with Human or RandomComputerPlayer.");
                System.exit(0);    
-            }//switch
+            }//switch, assigns players, exits game if invalid args given
             
    }//for loop
    
    System.out.println("\nWelcome to Reversi!  Moves should be entered in \"[row] [column]\" format.\n");
-  
-   
-   /** THE TESTING AREA 
-   
-   player1 = new RandomComputerPlayer("X");
-   ReversiBoard itsaboard = new ReversiBoard();
-   itsaboard.whereAmI(player1.getPlayer());
-   
-   itsaboard.printBoard();
-    
-   for(int i = 0; i<itsaboard.whereICanGo(player1.getPlayer()).size();i++){
-   AHAHA  = itsaboard.whereICanGo(player1.getPlayer()).get(i);
-   System.out.print((AHAHA[0]+1) + ", ");
-   System.out.println((AHAHA[1]+1));
-   }
-   
-   player1.setMove(itsaboard.whereICanGo(player1.getPlayer()));
-   System.out.println(player1.getMove()[0] + ", " +player1.getMove()[1]);*/
-   
      
+     
+   ReversiBoard itsaboard = new ReversiBoard();
    
    //below commented out until boolean wincondition actually exists
-   /*while(!wincondition){
+   while(!itsaboard.isGameOver(itsaboard.whereICanGo(player1.getPlayer()), itsaboard.whereICanGo(player2.getPlayer()))){
+ 
          itsaboard.printBoard(); 
-         if((count%2 == 0){
+         if(count%2 == 0){
             itsaboard.whereAmI(player2.getPlayer());
-            player2.setMove(itsaboard.whereICanGo(player2.getPlayer()));
-         }
+            if(itsaboard.whereICanGo(player2.getPlayer()).size() != 0){
+               player2.setMove(itsaboard.whereICanGo(player2.getPlayer()));
+            }//if, skips player turn if they don't have any possible moves
+            else{
+               if(player2 instanceof HumanPlayer){
+                  System.out.println("No possible moves!");
+               }//if human player, displays message for them if they had no possible moves
+            }
+         }//if
          else{
             itsaboard.whereAmI(player1.getPlayer());
-            player1.setMove(itsaboard.whereICanGo(player1.getPlayer()));
-         }
+            if(itsaboard.whereICanGo(player1.getPlayer()).size() != 0){
+               player1.setMove(itsaboard.whereICanGo(player1.getPlayer()));
+            }//if, sets player move if they have possible moves
+            else{
+               if(player1 instanceof HumanPlayer){
+                  System.out.println("No possible moves!");
+               }//if human player, displays message for them if they had no possible moves
+            }//else
+         }//tests to see which player's turn it is. X goes on odd numbered turns, O on even
          count++;
-   }
+   }//while, runs while game end conditions not met
    
-   some method that counts the amount of 'O's and 'X's on the board to determine winner
    
-   */
    
-   System.out.println("GG NUBS -insertplayer- WINS");
+   itsaboard.displayScore();
   }
 }
